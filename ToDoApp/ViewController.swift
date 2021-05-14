fec7df66bcbeb5a9a1c0b8f7523b3f33560d6c8c
Satinder Panesar <satinder@vigsitainteractives.com>
2021-05-14 22:34:29 +0530

    added some code in task view controller class file.

--- ToDoApp/ViewController.swift
+++ ToDoApp/ViewController.swift
@@ -6,14 +6,53 @@
 //
 
 import UIKit
+import FirebaseDatabase
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController{
+    @IBOutlet weak var tblview: UITableView!
+    
+    var arrTaskData = [String]()
+    var databaseHandler: DatabaseHandle?
+    var ref: DatabaseReference?
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // set the firebase referance
+        ref = Database.database().reference()
+        
+        // retrieve the post and listen for changes
+        
+       databaseHandler = ref?.child("task").observe(.childAdded, with: { (Snapshot) in
+             
+            // code to execute when child is added under posts
+        
+        let task = Snapshot.value as? String
+        
+        if let actualTask = task{
+            self.arrTaskData.append(actualTask)
+            self.tblview.reloadData()
+         }
+       })
     }
 
-
+    @IBAction func btnAddTaskView(_ sender: Any) {
+        
+    }
+    
 }
 
+extension ViewController: UITableViewDelegate,UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arrTaskData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tblview.dequeueReusableCell(withIdentifier: "taskCell")
+        cell?.textLabel?.text = arrTaskData[indexPath.row]
+        return cell!
+    }
+    
+    
+    
+}
